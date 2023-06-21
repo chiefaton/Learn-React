77ec892040c6719a4524e3073c2249bf023a5837
Chief <Manikandan.M@lntinfotech.com>
2023-06-21 10:20:51 +0530

    Learn react initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+//eactDOM.render(<p>Hello world from React</p> , document.getElementById("root"));
+
+ReactDOM.render(
+    <div>
+        <h1>React Course</h1>
+        <p>We will learn react this Course</p>
+        </div>,
+        document.getElementById("root")
+);
+
+//what we want to render 
+//where we want to render
